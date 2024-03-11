7a832878a35bf025cbed3bef2db4b3271de783a9
robel <robelwon75@gmail.com>
2024-03-11 21:40:14 +0300

    routes fix

--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,25 @@
 import express from 'express';
-
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-
 const router = express.Router();
 
-const routeController = (app) => {
-  app.use('/', router);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-  router.get('/files/:id', (req, res) => {
-    FilesController.getShow(req, res);
-  });
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
-  router.get('/files', (req, res) => {
-    FilesController.getIndex(req, res);
-  });
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
-  router.put('/files/:id/publish', (req, res) => {
-    FilesController.putPublish(req, res);
-  });
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
-  router.put('/files/:id/unpublish', (req, res) => {
-    FilesController.putUnpublish(req, res);
-  });
-
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
-
-  router.get('/connect', (req, res) => {
-    AuthController.getConnect(req, res);
-  });
-
-  router.get('/disconnect', (req, res) => {
-    AuthController.getDisconnect(req, res);
-  });
-
-  router.get('/users/me', (req, res) => {
-    UsersController.getMe(req, res);
-  });
-
-  router.post('/files', (req, res) => {
-    FilesController.postUpload(req, res);
-  });
-
-  router.post('/files/:id/data', (req, res) => {
-    FilesController.getFile(req, res);
-  });
-
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
-
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
-};
-
-export default routeController;
+export default router;
