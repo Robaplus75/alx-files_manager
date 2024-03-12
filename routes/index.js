ba7e9215147edc1146829a84f0f12893dc382447
robel <robelwon75@gmail.com>
2024-03-12 22:11:00 +0300

    route eslint

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
