f8d0d8a98bc189ae8cfccc22675492e84914edd1
robel <robelwon75@gmail.com>
2024-03-12 22:06:06 +0300

    route fix

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ import AuthController from '../controllers/AuthController';
 const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
